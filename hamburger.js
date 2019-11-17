a885a00d365c6dfea6137deb51979b9541fc4240
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-17 17:29:49 +0000

    almost done except CV download

--- hamburger.js
+++ hamburger.js
@@ -21,22 +21,4 @@ function navScroll() {
     }
 }
 
-//get button
-var emailButton = document.querySelector('.email')
-//add event listener
-    emailButton.addEventListener('click', function(e) {
-        //find the element that has the text you want.
-        var content = e.target.textContent;
-
-        // try {
-        //     // Now that we've selected the text, execute the copy command
-        // var copyTo = document.createElement('<textarea>')
-        // copyTo.value = emailButton
-        // document.body.appendChild(copyTo)
-        // copyTo.select()
-        document.execCommand('copy')
-        console.log(content)
-        // document.removeChild(copyTo)
-    })
-
 hamburgerToggle()
